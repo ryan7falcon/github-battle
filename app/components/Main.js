fb232f41dbeff30269989cceff4f2312bdab1cb2
Ryan West <ryanwest_ca@yahoo.com>
2016-06-09 10:27:19 -0400

    react-addons-css-transition-group, import/export to es6

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default React.createClass({
 	render() {
 		return (
 			<div className="main-container">
-				{this.props.children}
+				<ReactCSSTransitionGroup
+                    transitionName="appear"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}
+                    >
+                    { React.cloneElement(this.props.children, {key: this.props.location.pathname}) }
+                </ReactCSSTransitionGroup>
 			</div>
 		);
 	}
